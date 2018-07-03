4dda4c3009ff3e570b4c97d3c37370ab9d1509dc
DenysenkoSergei <denysenkosergei@gmail.com>
2018-07-03 18:21:29 +0300

    Initial configuration

---
+++ web/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'StreamForge';
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('application')
+);
---
+++ web/webpack.config.js
@@ -0,0 +1,27 @@
+const relPath = '/../src/main/resources/static/build';
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + relPath,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};

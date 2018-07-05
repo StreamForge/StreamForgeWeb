a28f4c6cd9c3f86862daa9cc46426dbcb17c3587
SFDevelopement <40800546+SFDevelopement@users.noreply.github.com>
2018-07-05 11:20:31 +0300

    Update index.js

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'StreamForge Test Title';
+const title = 'StreamForge Test Title, traveller!';
 
 ReactDOM.render(
     <div>{title}</div>,
